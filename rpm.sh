b4e282965144b1d7f67d02d8db0e3b4f913508e2
arfshl <98870352+arfshl@users.noreply.github.com>
2022-06-24 14:00:35 +0700

    Update rpm.sh

--- rpm.sh
+++ rpm.sh
@@ -6,7 +6,7 @@ cd LibreOffice*
 cd RPMS
 sudo rpm -i *.rpm
 rm *.rpm"
-read -p "Press any key to install"
+read -p "Press any key to install..."
 tar xzvf LibreOffice_*_Linux_x86-rpm*.tar.gz
 rm LibreOffice_*_Linux_x86-rpm*.tar.gz
 cd LibreOffice*
