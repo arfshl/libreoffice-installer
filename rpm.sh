8a4c6f060e89bacb0f6437e52b41a59016517e13
arfshl <98870352+arfshl@users.noreply.github.com>
2022-06-27 14:24:36 +0700

    Update rpm.sh

--- rpm.sh
+++ rpm.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "LibreOffice Installer"
+wget https://downloadarchive.documentfoundation.org/libreoffice/old/latest/rpm/x86_64/LibreOffice_7.3.4.2_Linux_x86-64_rpm.tar.gz
 tar xzvf LibreOffice_*_Linux_x86-rpm*.tar.gz
 rm LibreOffice_*_Linux_x86-rpm*.tar.gz
 cd LibreOffice*
