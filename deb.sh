db9507de9beb7018358ee380aad5bf503c91e36e
arfshl <98870352+arfshl@users.noreply.github.com>
2022-07-11 09:27:50 +0700

    Update deb.sh

--- deb.sh
+++ deb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "LibreOffice Installer"
 cd ~/
-wget https://downloadarchive.documentfoundation.org/libreoffice/old/latest/deb/x86_64/LibreOffice_7.3.4.2_Linux_x86-64_deb.tar.gz -P ~/
+wget https://downloadarchive.documentfoundation.org/libreoffice/old/latest/deb/x86_64/LibreOffice_7.4.0.1_Linux_x86-64_deb.tar.gz -P ~/
 tar xzvf LibreOffice_*_Linux_x86-64_deb*.tar.gz
 rm LibreOffice_*_Linux_x86-64_deb*.tar.gz
 cd LibreOffice*
